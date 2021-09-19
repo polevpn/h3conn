956c4f628d2978f685b7793f9b3c7a95623f1b81
starjiang <starjiang1984@gmail.com>
2021-09-19 20:11:11 +0800

    add NewClient function

--- client.go
+++ client.go
@@ -12,22 +12,20 @@ import (
 	"github.com/lucas-clemente/quic-go/http3"
 )
 
-// Client provides HTTP2 client side connection with special arguments
 type Client struct {
-	// Header enables sending custom headers to the server
-	Header http.Header
-	// Client is a custom HTTP client to be used for the connection.
-	// The client must have an http2.Transport as it's transport.
-	Client *http.Client
+	RoundTripper *http3.RoundTripper
 }
 
-// Connect establishes a full duplex communication with an HTTP2 server with custom client.
-// See h2conn.Connect documentation for more info.
+func NewClient(verifySSL bool) *Client {
+	client := &Client{
+		RoundTripper: &http3.RoundTripper{TLSClientConfig: &tls.Config{InsecureSkipVerify: verifySSL}},
+	}
+	return client
+}
+
 func (c *Client) Connect(urlStr string, timeout time.Duration) (*Conn, *http.Response, error) {
 	reader, writer := io.Pipe()
-
 	// Create a request object to send to the server
-
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 
@@ -36,16 +34,6 @@ func (c *Client) Connect(urlStr string, timeout time.Duration) (*Conn, *http.Res
 		return nil, nil, err
 	}
 
-	// Apply custom headers
-	if c.Header != nil {
-		req.Header = c.Header
-	}
-
-	// If an http client was not defined, use the default http client
-	httpClient := c.Client
-	if httpClient == nil {
-		httpClient = defaultClient.Client
-	}
 	timer := time.NewTimer(timeout)
 
 	go func() {
@@ -54,7 +42,7 @@ func (c *Client) Connect(urlStr string, timeout time.Duration) (*Conn, *http.Res
 	}()
 
 	// Perform the request
-	resp, err := httpClient.Do(req)
+	resp, err := c.RoundTripper.RoundTrip(req)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -70,9 +58,7 @@ func (c *Client) Connect(urlStr string, timeout time.Duration) (*Conn, *http.Res
 }
 
 var defaultClient = Client{
-	Client: &http.Client{
-		Transport: &http3.RoundTripper{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
-	},
+	RoundTripper: &http3.RoundTripper{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}},
 }
 
 func Connect(urlStr string, timeout time.Duration) (*Conn, *http.Response, error) {
